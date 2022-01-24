f7091554bd25d469273d605d0d9cff6b60247a75
kizuna24 <kizuna299@gmail.com>
2022-01-24 22:14:40 +0800

    menambahkan navbar dan footer

--- user/index.php
+++ user/index.php
@@ -67,8 +67,24 @@
 
 	}
 	?>
+
+
+	</footer>
 	  
 	</body>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>Copyright &copy;2022 Kuvukiland Company. All rights reserved. By <a href="http://bootstraptaste.com"></a></p>
+				</div>
+				<!-- 
+                    All links in the footer should remain intact. 
+                    Licenseing information is available at: http://bootstraptaste.com/license/
+                    You can buy this theme without footer links online at: http://bootstraptaste.com/buy/?theme=Mamba
+                -->
+			</div>
+		</div>
 
 	<!-- js -->
     <script src="../js/jquery.js"></script>
