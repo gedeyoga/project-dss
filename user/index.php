bdeefa3afafb1e562c03a19f5932c7b5f1b0f191
kizuna24 <kizuna299@gmail.com>
2022-01-24 19:49:49 +0800

    menambahkan navbar

--- user/index.php
+++ user/index.php
@@ -18,7 +18,7 @@
 	<script src="../js/modernizr.custom.js"></script>
 	</head>
   <body>
-	<div class="menu-area">
+	<!-- <div class="menu-area">
 			<div id="dl-menu" class="dl-menuwrapper">
 						<button class="dl-trigger">Open Menu</button>
 						<ul class="dl-menu">
@@ -28,8 +28,37 @@
 							<li><a href="?page=pengumuman">Pengumuman</a></li>
 							<li><a href="logout_user.php">Logout</a></li>
 						</ul>
-					</div><!-- /dl-menuwrapper -->
-	</div>	
+					</div>/dl-menuwrapper -->
+
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand scroll-page" href="#">Kuvukiland Tech</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#intro">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="?page=download">Download</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="?page=penerimaan">Penerimaan</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="?page=pengumuman">Pengumuman</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link"href="logout_user.php">Logout</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#contact">Kontak</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
 
 	<?php
 		include "page.php";
